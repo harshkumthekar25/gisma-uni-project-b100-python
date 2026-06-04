96c7d5f5f31c1c8fe350e7a4a309c76e682ffdfc
harshkumthekar25 <harshkumthekar25@gmail.com>
2026-06-04 14:47:56 +0200

    Document project module structure in main.py
    Add module structure documentation for the project.

---
+++ main.py
@@ -0,0 +1,10 @@
+module structure: 
+capital.com/_project/
+
+  main.py                     # main program entry point
+  asset.py                    # asset, stock, crypto classes
+  portfolio.py                # Portfolio class
+  file_handler.py             # File I/0 operations
+  utils.py                    # utility functions
+  data/
+  
